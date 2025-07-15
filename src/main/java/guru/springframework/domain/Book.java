d3389ece72ee55d23ac6ec26aff33d9631c58c47
Ech-Cherrate Ismail <echcherrate.ismail@gmail.com>
2025-07-15 22:12:26 +0200

    Added Book, Author entities

---
+++ src/main/java/guru/springframework/domain/Book.java
@@ -0,0 +1,59 @@
+package guru.springframework.domain;
+
+/*
+ * @author Ech-Cherrate Ismail
+ * @project spring-6-webapp
+ * @create 15/07/2025 - 01:45
+ */
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String isbn;
+
+    @ManyToMany
+    @JoinTable(name = "author_book",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id") )
+    private Set<Author> authors;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+}
